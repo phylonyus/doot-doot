f88e4e8cbdecd67bb4f9a072451f73c51d0d9919
vicelversa <robbie.pearson@gmail.com>
2019-11-19 20:14:33 -0700

    re-enable logging

--- doot-doot/utils/Logger.py
+++ doot-doot/utils/Logger.py
@@ -39,30 +39,30 @@ async def log(message, bot, level="INFO", debug=""):
     pass
     # lmao turn off logging
     # Return if the logging level is not DEBUG, and the bot is trying to log some debugging stuff
-    # logger = logging.getLogger("doot-doot")
-    # if (logger.getEffectiveLevel != logging.DEBUG) and (level == "DEBUG"):
-    #     return
+    logger = logging.getLogger("doot-doot")
+    if (logger.getEffectiveLevel != logging.DEBUG) and (level == "DEBUG"):
+        return
 
-    # logChannel = bot.get_channel(int(config['log_channel']))
-    # time = datetime.datetime.now().strftime('%H:%M:%S')
+    logChannel = bot.get_channel(int(config['log_channel']))
+    time = datetime.datetime.now().strftime('%H:%M:%S')
 
-    # if level == "ERROR":
-    #     levelemote = "❌"
-    # elif level == "CRITICAL":
-    #     levelemote = "🔥"
-    # elif level == "WARNING":
-    #     levelemote = "❗"
-    # elif level == "DEBUG":
-    #     levelemote = "🔧"
-    # else:
-    #     levelemote = "🔎"
+    if level == "ERROR":
+        levelemote = "❌"
+    elif level == "CRITICAL":
+        levelemote = "🔥"
+    elif level == "WARNING":
+        levelemote = "❗"
+    elif level == "DEBUG":
+        levelemote = "🔧"
+    else:
+        levelemote = "🔎"
 
-    # await logChannel.send("`[" + time + "]` **" + levelemote + " " + level + ":** " + message)
+    await logChannel.send("`[" + time + "]` **" + levelemote + " " + level + ":** " + message)
 
-    # if debug == "":
-    #     logDebug(message, level)
-    #     return
-    # logDebug(debug, level)
+    if debug == "":
+        logDebug(message, level)
+        return
+    logDebug(debug, level)
 
 
 def logDebug(message, level="INFO"):
@@ -83,11 +83,11 @@ def logDebug(message, level="INFO"):
 async def logCommand(commandName, ctx, level="INFO"):
     pass
     # lol, turn off logging
-    # if isinstance(ctx.message.channel, discord.DMChannel):
-    #     await log(ctx.author.name + "#" + ctx.author.discriminator + " just ran the " + commandName + " command",
-    #               ctx.bot, level)
-    # else:
-    #     await log(
-    #         ctx.author.name + "#" + ctx.author.discriminator + " just ran the " + commandName + " command, in the channel #" + ctx.channel.name + " (`" + str(
-    #             ctx.channel.id) + "`), in the guild " + ctx.guild.name + " (`" + str(ctx.guild.id) + "`)", ctx.bot,
-    #         level)
+    if isinstance(ctx.message.channel, discord.DMChannel):
+        await log(ctx.author.name + "#" + ctx.author.discriminator + " just ran the " + commandName + " command",
+                  ctx.bot, level)
+    else:
+        await log(
+            ctx.author.name + "#" + ctx.author.discriminator + " just ran the " + commandName + " command, in the channel #" + ctx.channel.name + " (`" + str(
+                ctx.channel.id) + "`), in the guild " + ctx.guild.name + " (`" + str(ctx.guild.id) + "`)", ctx.bot,
+            level)
