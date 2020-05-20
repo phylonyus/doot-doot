2e572579d2b41eb192d1eae3d80e0f216a6fc4ca
Jnathan0 <55114731+Jnathan0@users.noreply.github.com>
2020-05-19 17:59:53 -0700

    Add Sound Method
    Added sound method that sends private message to the user that calls it with a pretty print, tree style output of all the categories and their sounds. Added code for class DisplayablePath.

--- doot-doot/cogs/Airhorn.py
+++ doot-doot/cogs/Airhorn.py
@@ -19,6 +19,84 @@ sub_cmd_sep = config['sub_cmd_sep']
 
 voice_channel = None
 
+############################################################### Define class for pretty printing
+class DisplayablePath(object):
+    display_filename_prefix_middle = '├──'
+    display_filename_prefix_last = '└──'
+    display_parent_prefix_middle = '    '
+    display_parent_prefix_last = '│   '
+
+    def __init__(self, path, parent_path, is_last):
+        self.path = Path(str(path))
+        self.parent = parent_path
+        self.is_last = is_last
+        if self.parent:
+            self.depth = self.parent.depth + 1
+        else:
+            self.depth = 0
+
+    @property
+    def displayname(self):
+        if self.path.is_dir():
+            return self.path.name + '/'
+        return self.path.name
+
+    @classmethod
+    def make_tree(cls, root, parent=None, is_last=False, criteria=None):
+        root = Path(str(root))
+        criteria = criteria or cls._default_criteria
+
+        displayable_root = cls(root, parent, is_last)
+        yield displayable_root
+
+        children = sorted(list(path
+                               for path in root.iterdir()
+                               if criteria(path)),
+                          key=lambda s: str(s).lower())
+        count = 1
+        for path in children:
+            is_last = count == len(children)
+            if path.is_dir():
+                yield from cls.make_tree(path,
+                                         parent=displayable_root,
+                                         is_last=is_last,
+                                         criteria=criteria)
+            else:
+                yield cls(path, displayable_root, is_last)
+            count += 1
+
+    @classmethod
+    def _default_criteria(cls, path):
+        return True
+
+    @property
+    def displayname(self):
+        if self.path.is_dir():
+            return self.path.name + '/'
+        return self.path.name
+
+    def displayable(self):
+        if self.parent is None:
+            return self.displayname
+
+        _filename_prefix = (self.display_filename_prefix_last
+                            if self.is_last
+                            else self.display_filename_prefix_middle)
+
+        parts = ['{!s} {!s}'.format(_filename_prefix,
+                                    self.displayname)]
+
+        parent = self.parent
+        while parent and parent.parent is not None:
+            parts.append(self.display_parent_prefix_middle
+                         if parent.is_last
+                         else self.display_parent_prefix_last)
+            parent = parent.parent
+
+        return ''.join(reversed(parts))
+
+###############################################################
+
 # defining function to handle playing sounds in Voice Channel
 async def play_file(ctx, filename):
     if not ctx.author.voice:
@@ -139,6 +217,38 @@ class Airhorn(commands.Cog):
         else:
             file_path = alias_entry
         await play_file(ctx, file_path)
+       
+    
+    @commands.command()
+    @commands.guild_only()
+    async def sounds(self, ctx):
+        """View a list of categories and their sounds"""
+        user = ctx.message.author
+        paths = DisplayablePath.make_tree(Path(sounds_path))
+        msg_limit = 2000
+        size = 0
+        message = []
+        msg = "```"
+        for path in paths:
+            message.append(path.displayable())
+        #print(len(message))
+        for i in range(0, len(message)):
+            block = message[i]
+            #print(block)
+            if size+(len(block)+4) <= msg_limit:
+                msg+=('\n'+block)
+                #print('a')
+                size += len(block)+4
+                if i == len(message)-1:
+                    msg+="""```"""
+                    await user.send(msg)
+                #print(size)
+            elif size+len(block) > msg_limit:
+                msg+="""```"""
+                await user.send(msg)
+                #print(len(msg))
+                msg="```"
+                size=0
 
     @commands.command()
     @commands.guild_only()
